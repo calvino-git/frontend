18e82cc125ea2c1deceef80e0868389d5cfff51d
Calvin ILOKI <bbbebe661@yahoo.fr>
2025-08-30 06:44:55 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: "http://localhost:8080/api/v1",
+  authURL: "http://localhost:8080",
+  jwt: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYWx2aW4uaWxva2lAeWFob28uZnIiLCJpYXQiOjE3NTYzNzY0NTksImV4cCI6MTc1NjQxMjQ1OX0.NbeSgRebPRA69Tc2KuOiOTlNRqckwNyXzfBjbkIspUw"
 };
